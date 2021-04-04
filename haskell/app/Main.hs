1932da3e68fbf683bf2336af89473a040e312e3a
Jeffrey Pratt <clementi@users.noreply.github.com>
2021-04-04 16:36:46 -0600

    Alias: RNG -> Gen RealWorld

--- haskell/app/Main.hs
+++ haskell/app/Main.hs
@@ -5,6 +5,8 @@ import System.Random.MWC
 import Data.Word ( Word64 )
 import GHC.Exts ( RealWorld )
 
+type RNG = Gen RealWorld
+
 main :: IO ()
 main = do
   let pairCount = 1000000
@@ -13,19 +15,19 @@ main = do
   estimate <- averageEstimate g estimateCount pairCount
   putStrLn $ "Mean: " <> show estimate
 
-averageEstimate :: Gen RealWorld -> Int -> Int -> IO Double
+averageEstimate :: RNG -> Int -> Int -> IO Double
 averageEstimate g estimateCount pairCount = do
   estimates <- makeEstimates g estimateCount pairCount
   return (sum estimates / fromIntegral (length estimates))
 
-makeEstimates :: Gen RealWorld -> Int -> Int -> IO [Double]
+makeEstimates :: RNG -> Int -> Int -> IO [Double]
 makeEstimates _ 0 _ = return []
 makeEstimates g estimateCount pairCount = do
   estimate <- estimatePi g pairCount
   putStrLn $ "Estimate: " <> show estimate
   fmap (estimate :) (makeEstimates g (estimateCount - 1) pairCount)
 
-estimatePi :: Gen RealWorld -> Int -> IO Double
+estimatePi :: RNG -> Int -> IO Double
 estimatePi g pairCount = do
   pairs <- makePairs g pairCount
   let coprimeCount = length $ filter (uncurry coprime) pairs
@@ -33,15 +35,16 @@ estimatePi g pairCount = do
       estimate = sqrt $ 6 / probability
   return estimate
 
-makePairs :: Gen RealWorld -> Int -> IO [(Word64, Word64)]
+makePairs :: RNG -> Int -> IO [(Word64, Word64)]
 makePairs _ 0 = return []
 makePairs g pairCount = do
   pair <- makePair g
   fmap (pair :) (makePairs g (pairCount - 1))
 
-makePair :: Gen RealWorld -> IO (Word64, Word64)
+makePair :: RNG -> IO (Word64, Word64)
 makePair g = pairSequence (uniform g, uniform g)
   where pairSequence = uncurry $ liftM2 (,)
 
 coprime :: Word64 -> Word64 -> Bool
 coprime a b = gcd a b == 1
+
