3f1f291cb3369eccfb15ba112202a15bf574495c
Jeffrey Pratt <clementi@users.noreply.github.com>
2021-04-04 16:33:16 -0600

    Replace random with mwc-random, which is faster

--- haskell/app/Main.hs
+++ haskell/app/Main.hs
@@ -1,44 +1,47 @@
 module Main where
 
 import Control.Monad ( liftM2 )
-import System.Random ( Random(randomIO) )
+import System.Random.MWC
+import Data.Word ( Word64 )
+import GHC.Exts ( RealWorld )
 
 main :: IO ()
 main = do
   let pairCount = 1000000
   let estimateCount = 100
-  estimate <- averageEstimate estimateCount pairCount
+  g <- createSystemRandom
+  estimate <- averageEstimate g estimateCount pairCount
   putStrLn $ "Mean: " <> show estimate
 
-averageEstimate :: Int -> Int -> IO Double
-averageEstimate estimateCount pairCount = do
-  estimates <- makeEstimates estimateCount pairCount
+averageEstimate :: Gen RealWorld -> Int -> Int -> IO Double
+averageEstimate g estimateCount pairCount = do
+  estimates <- makeEstimates g estimateCount pairCount
   return (sum estimates / fromIntegral (length estimates))
 
-makeEstimates :: Int -> Int -> IO [Double]
-makeEstimates 0 _ = return []
-makeEstimates estimateCount pairCount = do
-  estimate <- estimatePi pairCount
+makeEstimates :: Gen RealWorld -> Int -> Int -> IO [Double]
+makeEstimates _ 0 _ = return []
+makeEstimates g estimateCount pairCount = do
+  estimate <- estimatePi g pairCount
   putStrLn $ "Estimate: " <> show estimate
-  fmap (estimate :) (makeEstimates (estimateCount - 1) pairCount)
+  fmap (estimate :) (makeEstimates g (estimateCount - 1) pairCount)
 
-estimatePi :: Int -> IO Double
-estimatePi pairCount = do
-  pairs <- makePairs pairCount
+estimatePi :: Gen RealWorld -> Int -> IO Double
+estimatePi g pairCount = do
+  pairs <- makePairs g pairCount
   let coprimeCount = length $ filter (uncurry coprime) pairs
       probability = fromIntegral coprimeCount / fromIntegral pairCount
       estimate = sqrt $ 6 / probability
   return estimate
 
-makePairs :: Int -> IO [(Int, Int)]
-makePairs 0 = return []
-makePairs pairCount = do
-  pair <- makePair
-  fmap (pair :) (makePairs (pairCount - 1))
+makePairs :: Gen RealWorld -> Int -> IO [(Word64, Word64)]
+makePairs _ 0 = return []
+makePairs g pairCount = do
+  pair <- makePair g
+  fmap (pair :) (makePairs g (pairCount - 1))
 
-makePair :: IO (Int, Int)
-makePair = pairSequence (randomIO, randomIO)
+makePair :: Gen RealWorld -> IO (Word64, Word64)
+makePair g = pairSequence (uniform g, uniform g)
   where pairSequence = uncurry $ liftM2 (,)
 
-coprime :: Int -> Int -> Bool
+coprime :: Word64 -> Word64 -> Bool
 coprime a b = gcd a b == 1
