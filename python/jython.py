a311aba2a5ca1ed41f26ef8f64567f6d86c904cf
Jeffrey Pratt <clementi@users.noreply.github.com>
2021-04-15 11:44:49 -0600

    Add Jython

---
+++ python/jython.py
@@ -0,0 +1,36 @@
+import math
+import random
+
+limit = 1000000000
+
+def pairs(count):
+    return map(lambda _: (random.randint(1, limit), random.randint(1, limit)) , range(count))
+
+def gcd(a, b):
+    if b == 0:
+        return a
+    return gcd(b, a % b)
+
+coprime_count = 0
+
+pair_count = 1000000
+estimate_count = 100
+
+estimate_sum = 0
+
+for i in range(0, estimate_count):
+    for (a, b) in pairs(pair_count):
+        if gcd(a, b) == 1:
+            coprime_count += 1
+
+    coprime_probability = float(coprime_count) / float(pair_count)
+
+    estimate = math.sqrt(6.0 / coprime_probability)
+    estimate_sum += estimate
+    coprime_count = 0
+
+    print "Estimate %s: %s" % (i, estimate)
+
+average_estimate = estimate_sum / estimate_count
+
+print "Mean: %s" % average_estimate
