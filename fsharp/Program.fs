7365a7688ac9be90f4582da63798a3ab30455f82
Jeffrey Pratt <clementi@users.noreply.github.com>
2021-05-29 15:06:46 -0600

    Use faster algo for F#

--- fsharp/Program.fs
+++ fsharp/Program.fs
@@ -1,38 +1,26 @@
 open System
 
-let createRandomPair (rng: Random) =
-    (rng.Next (), rng.Next ())
-
-let rec createPairs pairCount rng =
-    seq { 0 .. pairCount - 1 } |> Seq.map (fun _ -> createRandomPair rng) |> Seq.toList
-
 let rec gcd a b =
     match b with
     | 0 -> a
     | c -> gcd c (a % c)
 
-let coprime (a, b) = gcd a b = 1
+let coprime a b = gcd a b = 1
 
-let estimatePi pairCount =
-    let rng = new Random ()
-    let pairs = createPairs pairCount rng
-    let coprimeCount = pairs |> List.filter coprime |> List.length
+let estimatePi i (rng: Random) pairCount =
+    let coprimeCount = seq { 0 .. pairCount - 1 } |> Seq.map (fun _ -> if coprime (rng.Next ()) (rng.Next ()) then 1 else 0) |> Seq.sum
     let probability = float coprimeCount / float pairCount
-    Math.Sqrt (6.0 / probability)
-
-let estimateAndShow pairCount i =
-    let estimate = estimatePi pairCount
+    let estimate = Math.Sqrt (6.0 / probability)
     printfn "Estimate %d: %f" i estimate
     estimate
 
-let averageEstimate estimateCount pairCount =
-    let estimates = seq { 0 .. estimateCount - 1 } |> Seq.toList |> List.map (estimateAndShow pairCount)
-    float (List.sum estimates) / float estimateCount
-
 [<EntryPoint>]
 let main _ =
-    let pairCount = 1000000
+    let pairCount = 1_000_000
     let estimateCount = 100
-    let estimate = averageEstimate estimateCount pairCount
-    printfn "Mean: %f" estimate
+
+    let rng = new Random ()
+    let estimates = seq { 0 .. estimateCount - 1 } |> Seq.map (fun i -> estimatePi i rng pairCount)
+
+    printfn "Mean: %f" ((Seq.sum estimates) / float estimateCount)
     0
