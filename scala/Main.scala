71725a07d5e266919cbab2e6e39e65617a139885
Jeff Pratt <clementi@users.noreply.github.com>
2025-04-01 17:07:49 -0600

    Eliminate Scala 2 projects

--- scala/Main.scala
+++ scala/Main.scala
@@ -1,7 +1,7 @@
 import math.sqrt
 import scala.util.Random
 
-object Main extends App {
+@main def main: Unit =
   val pairCount = 1_000_000
   val estimateCount = 100
 
@@ -13,27 +13,21 @@ object Main extends App {
 
   println(s"Mean: ${estimates.sum / estimateCount}")
 
-  private def estimatePi(i: Int, rng: Random, pairCount: Int): Double = {
-    val coprimeCount = countCoprime(rng, pairCount)
-    val probability = coprimeCount.toDouble / pairCount
-    val estimate = sqrt(6 / probability)
-    println(s"Estimate $i: $estimate")
-    estimate
-  }
+private def estimatePi(i: Int, rng: Random, pairCount: Int): Double =
+  val coprimeCount = countCoprime(rng, pairCount)
+  val probability = coprimeCount.toDouble / pairCount
+  val estimate = sqrt(6 / probability)
+  println(s"Estimate $i: $estimate")
+  estimate
 
-  private def countCoprime(rng: Random, pairCount: Int): Int = {
-    if (pairCount == 0)
-      0
-    else if (coprime(rng.nextInt, rng.nextInt))
-      1 + countCoprime(rng, pairCount - 1)
-    else
-      countCoprime(rng, pairCount - 1)
-  }
+private def countCoprime(rng: Random, pairCount: Int): Int =
+  Iterator.continually((rng.nextInt(Int.MaxValue), rng.nextInt(Int.MaxValue)))
+    .take(pairCount)
+    .filter(coprime)
+    .size
 
-  private def coprime(a: Int, b: Int): Boolean = gcd(a, b) == 1
+private def coprime(a: Int, b: Int): Boolean = gcd(a, b) == 1
 
-  private def gcd: (Int, Int) => Int = {
-    case (a, 0) => a
-    case (a, b) => gcd(b, a % b)
-  }
-}
+private def gcd: (Int, Int) => Int =
+  case (a, 0) => a
+  case (a, b) => gcd(b, a % b)
