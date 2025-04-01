0ddc526ec7660123655166eebcbc601a0297e51c
Jeff Pratt <clementi@users.noreply.github.com>
2025-04-01 17:17:48 -0600

    Eliminate Scala 2 projects

--- zig/src/main.zig
+++ zig/src/main.zig
@@ -7,22 +7,26 @@ pub fn main() !void {
     var coprime_count: u32 = 0;
     var estimate_sum: f64 = 0.0;
 
-    var rng = std.rand.DefaultPrng.init(std.time.nanoTimestamp());
+    var rng = std.rand.DefaultPrng.init(blk: {
+        var seed: u64 = undefined;
+        try std.posix.getrandom(std.mem.asBytes(&seed));
+        break :blk seed;
+    });
 
     for (0..estimate_count) |i| {
         for (0..pair_count) |_| {
-            if (coprime(rng.random(), rng.random())) {
+            if (coprime(rng.random().int(u32), rng.random().int(u32))) {
                 coprime_count += 1;
             }
         }
-        const probability = @intToFloat(f64, coprime_count) / @intToFloat(f64, pair_count);
+        const probability = @as(f64, @floatFromInt(coprime_count)) / @as(f64, @floatFromInt(pair_count));
         const estimate = @sqrt(6.0 / probability);
-        std.debug.print("Estimate {}: {}\n", .{i, estimate});
+        std.debug.print("Estimate {}: {}\n", .{ i, estimate });
         estimate_sum += estimate;
         coprime_count = 0;
     }
 
-    std.debug.print("Mean: {}\n", .{estimate_sum / @intToFloat(f64, estimate_count)});
+    std.debug.print("Mean: {}\n", .{estimate_sum / @as(f64, @floatFromInt(estimate_count))});
 }
 
 fn coprime(a: u32, b: u32) bool {
