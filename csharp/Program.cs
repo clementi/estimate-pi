8931e1d1edb55d3d8c1b4ea0ca6656d83e3a651e
Jeff Pratt <clementi@users.noreply.github.com>
2025-04-02 19:34:59 -0600

    Update C# implementation

--- csharp/Program.cs
+++ csharp/Program.cs
@@ -2,44 +2,41 @@
 using System.Collections.Generic;
 using System.Linq;
 
-namespace csharp
+var pairCount = 1_000_000;
+var estimateCount = 100;
+
+var coprimeCount = 0;
+
+var estimateSum = 0.0;
+
+var rng = new Random();
+
+for (var i = 0; i < estimateCount; i++)
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            int pairCount = 1_000_000;
-            int estimateCount = 100;
+    coprimeCount = RandomPairs(rng).Take(pairCount).Where(Coprime).Count();
+    var proportion = (double)coprimeCount / pairCount;
+    var estimate = Math.Sqrt(6 / proportion);
+    Console.WriteLine($"Estimate {i}: {estimate}");
+    estimateSum += estimate;
+    coprimeCount = 0;
+}
 
-            int coprimeCount = 0;
+Console.WriteLine($"Mean: {estimateSum / estimateCount}");
 
-            double sum = 0.0;
+static IEnumerable<(uint, uint)> RandomPairs(Random rng) {
+    while (true)
+        yield return ((uint)rng.Next(), (uint)rng.Next());
+}
 
-            var rng = new Random();
+static bool Coprime((uint, uint) pair)
+{
+    var (a, b) = pair;
+    return Gcd(a, b) == 1;
+}
 
-            for (int i = 0; i < estimateCount; i++)
-            {
-                for (int j = 0; j < pairCount; j++)
-                    if (Coprime((uint)rng.Next(), (uint)rng.Next()))
-                        coprimeCount++;
-
-                double probability = (double)coprimeCount / pairCount;
-                double estimate = Math.Sqrt(6 / probability);
-                Console.WriteLine("Estimate {0}: {1}", i, estimate);
-                sum += estimate;
-                coprimeCount = 0;
-            }
-
-            Console.WriteLine("Mean: {0}", sum / estimateCount);
-        }
-
-        private static bool Coprime(uint a, uint b) => Gcd(a, b) == 1;
-
-        private static ulong Gcd(uint a, uint b)
-        {
-            if (b == 0)
-                return a;
-            return Gcd(b, a % b);
-        }
-    }
+static uint Gcd(uint a, uint b)
+{
+    if (b == 0)
+        return a;
+    return Gcd(b, a % b);
 }
