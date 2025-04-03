43a3125b167648755393959aefb13e4a701c7db2
Jeff Pratt <clementi@users.noreply.github.com>
2025-04-02 20:08:27 -0600

    Use vars where possible

--- java/Main.java
+++ java/Main.java
@@ -1,27 +1,23 @@
 import java.util.Random;
-import java.util.stream.Stream;
 
 public class Main {
     public static void main(String[] args) {
-        int limit = 1_000_000_000;
+        var pairCount = 1_000_000;
+        var estimateCount = 100;
 
-        int pairCount = 1_000_000;
-        int estimateCount = 100;
+        var estimateSum = 0.0;
 
-        int coprimeCount = 0;
+        var rng = new Random();
 
-        double estimateSum = 0;
-
-        Random rng = new Random();
-
-        for (int i = 0; i < estimateCount; i++) {
-            for (int j = 0; j < pairCount; j++) {
-                if (coprime(rng.nextInt(limit), rng.nextInt(limit))) {
+        for (var i = 0; i < estimateCount; i++) {
+            var coprimeCount = 0;
+            for (var j = 0; j < pairCount; j++) {
+                if (coprime(rng.nextInt(Integer.MAX_VALUE), rng.nextInt(Integer.MAX_VALUE))) {
                     coprimeCount++;
                 }
             }
-            double probability = (double) coprimeCount / pairCount;
-            double estimate = Math.sqrt(6 / probability);
+            var probability = (double) coprimeCount / pairCount;
+            var estimate = Math.sqrt(6 / probability);
 
             System.out.printf("Estimate %d: %.15f%n", i, estimate);
             estimateSum += estimate;
