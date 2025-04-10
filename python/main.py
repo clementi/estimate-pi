4143834a861009fb33a8297ac2be24a97569a8f9
Jeff Pratt <clementi@users.noreply.github.com>
2025-04-10 17:48:24 -0600

    Use an inifnite sequence of pairs and itertools

--- python/main.py
+++ python/main.py
@@ -1,23 +1,22 @@
 import math
 import random
+import itertools
 
-limit = 1000000000
+limit: int = 1_000_000_000
 
-def pairs(count):
-    return map(lambda _: (random.randint(1, limit), random.randint(1, limit)) , range(count))
+def pairs():
+    while True:
+        yield (random.randint(1, limit), random.randint(1, limit))
 
-coprime_count = 0
+coprime_count: int = 0
 
-pair_count = 1000000
-estimate_count = 100
+pair_count: int = 1_000_000
+estimate_count: int = 100
 
-estimate_sum = 0
+estimate_sum: float = 0
 
 for i in range(0, estimate_count):
-    for (a, b) in pairs(pair_count):
-        if math.gcd(a, b) == 1:
-            coprime_count += 1
-
+    coprime_count = sum(1 for (a, b) in itertools.islice(pairs(), pair_count) if math.gcd(a, b) == 1)
     coprime_probability = coprime_count / pair_count
 
     estimate = math.sqrt(6 / coprime_probability)
