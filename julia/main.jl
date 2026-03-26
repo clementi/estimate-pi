d502c13373f2ca108959f5d15c2e15ace9d68477
Jeff Pratt <clementi@users.noreply.github.com>
2026-03-25 21:19:59 -0600

    Use Base.@main macro

--- julia/main.jl
+++ julia/main.jl
@@ -1,22 +1,24 @@
-pairCount = 1000000
-estimateCount = 100
+function (@main)(args)
+    pairCount = 1000000
+    estimateCount = 100
 
-coprimeCount = 0
-estimateSum = 0
+    global coprimeCount = 0
+    global estimateSum = 0
 
-for i in 1:estimateCount
-    for j in 1:pairCount
-        if gcd(rand(Int), rand(Int)) == 1
-            global coprimeCount += 1
+    for i in 1:estimateCount
+        for j in 1:pairCount
+            if gcd(rand(UInt32), rand(UInt32)) == 1
+                global coprimeCount += 1
+            end
         end
+
+        probability = coprimeCount / pairCount
+        estimate = sqrt(6 / probability)
+        println("Estimate ", i, ": ", estimate)
+
+        global estimateSum += estimate
+        global coprimeCount = 0
     end
 
-    probability = coprimeCount / pairCount
-    estimate = sqrt(6 / probability)
-    println("Estimate ", i, ": ", estimate)
-
-    global estimateSum += estimate
-    global coprimeCount = 0
+    println("Mean: ", estimateSum / estimateCount)
 end
-
-println("Mean: ", estimateSum / estimateCount)
