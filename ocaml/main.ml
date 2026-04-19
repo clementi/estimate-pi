0e26d0ae86c66499b1093ae1a7621e2b73a82d93
Jeff Pratt <clementi@users.noreply.github.com>
2026-04-18 21:29:34 -0600

    Use tail recursion with accumulators

--- ocaml/main.ml
+++ ocaml/main.ml
@@ -1,8 +1,3 @@
-let rec createPairs pairCount =
-  match pairCount with
-  | 0 -> []
-  | _ -> (Random.int 1000000000, Random.int 1000000000) :: createPairs (pairCount - 1)
-
 let rec gcd a b =
   match b with
   | 0 -> a
@@ -10,31 +5,29 @@ let rec gcd a b =
 
 let coprime a b = gcd a b = 1
 
-let estimatePi pairCount =
-  let pairs = createPairs pairCount in
-  let coprimeCount = List.length (List.filter (fun pair -> let a, b = pair in coprime a b) pairs) in
-  let proportion = (float coprimeCount) /. (float pairCount)
-  in Float.sqrt (6.0 /. proportion)
+let randInt () = Random.int 1000000000
 
-let estimateAndShow pairCount i =
-  let estimate = estimatePi pairCount in
-  let _ = Printf.printf ("Estimate %d: %.15f\n%!") i estimate
-  in estimate
+let rec countCoprime pairCount acc =
+  match pairCount with
+  | 0 -> acc
+  | _ -> let count = if coprime (randInt ()) (randInt ()) then 1 else 0 in
+    countCoprime (pairCount - 1) (count + acc)
 
-let rec makeEstimates estimateCount pairCount =
+let rec estimatePi pairCount =
+  let coprimeCount = countCoprime pairCount 0 in
+  let proportion = (float coprimeCount) /. (float pairCount) in
+  Float.sqrt (6.0 /. proportion)
+
+let rec sumEstimates i estimateCount pairCount acc =
   match estimateCount with
-  | 0 -> []
-  | i -> estimateAndShow pairCount i :: makeEstimates (estimateCount - 1) pairCount
+  | 0 -> acc
+  | _ -> let estimate = estimatePi pairCount in
+         let _ = Printf.printf "Estimate %d: %.15f\n%!" i estimate in
+         sumEstimates (i + 1) (estimateCount - 1) pairCount (estimate +. acc)
 
-let averageEstimate estimateCount pairCount =
-  let estimates = makeEstimates estimateCount pairCount
-  in (List.fold_left (+.) 0.0 estimates) /. (float (List.length estimates))
-
-let main () =
+let _ =
   let _ = Random.self_init in
   let pairCount = 1000000 in
-  let estimateCount = 99 in
-  let estimate = averageEstimate estimateCount pairCount
-  in Printf.printf ("Mean: %.15f\n") estimate
-
-let _ = main ()
+  let estimateCount = 100 in
+  let estimateSum = sumEstimates 0 estimateCount pairCount 0.0
+  in Printf.printf "Mean: %.15f\n%!" (estimateSum /. float estimateCount)
