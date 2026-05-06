ac3c3f0e8ead3908f539df43787f568659651333
Jeff Pratt <clementi@users.noreply.github.com>
2026-05-05 20:02:49 -0600

    Use Xorshift for Swift and simpler GCD

--- swift/main.swift
+++ swift/main.swift
@@ -1,28 +1,44 @@
-let limit = 1_000_000_000
+import Foundation
 
-var coprimeCount = 0
-let pairCount = 1_000_000
-let estimateCount = 100
-
-var estimateSum = 0.0
-
-func gcd(_ a: Int, _ b: Int) -> Int {
-    if (b == 0) {
-        return a
-    } else {
-        return gcd(b, a % b)
+struct Xorshift64: RandomNumberGenerator {
+    var state: UInt64
+    init(seed: UInt64 = 12345) { state = seed }
+    mutating func next() -> UInt64 {
+        state ^= state << 13
+        state ^= state >> 7
+        state ^= state << 17
+        return state
     }
 }
 
+func gcd(_ a: Int, _ b: Int) -> Int {
+    var a = a, b = b
+    while b != 0 {
+        let t = b
+        b = a % b
+        a = t
+    }
+    return a
+}
+
+let limit = 1_000_000_000
+let pairCount = 1_000_000
+let estimateCount = 100
+var estimateSum = 0.0
+
 for i in 1...estimateCount {
+    var rng = Xorshift64(seed: UInt64(i) &* 6364136223846793005)
+    var coprimeCount = 0
     for _ in 1...pairCount {
-        if gcd(Int.random(in: 1...limit), Int.random(in: 1...limit)) == 1 {
+        let a = Int(rng.next() % UInt64(limit)) + 1
+        let b = Int(rng.next() % UInt64(limit)) + 1
+        if gcd(a, b) == 1 {
             coprimeCount += 1
         }
     }
 
-    let probability = Double (coprimeCount) / Double (pairCount)
-    let estimate = (6.0 / probability).squareRoot()
+    let proportion = Double (coprimeCount) / Double (pairCount)
+    let estimate = (6.0 / proportion).squareRoot()
     estimateSum += estimate
     coprimeCount = 0
 
